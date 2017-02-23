102b0ce56bc485c914c322bb8d30b74ddd2c8689
joaopluigi <joao.luigi@omnilink.com.br>
2017-02-23 09:11:07 -0300

    updating usage instructions

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# Simple Python Makefile Generator
+# Makefile Generator
 
-Usage:                                         
-  .c files must be on ./src/                 
-  .h files must be on ./inc/                 
-  execute makefile_generator.py    
+Simple and easy makefile generator made in Python!
+
+### Usage:           
+  - set execution permission
+  - run makefile_generator.py
+  - make
+  - binary file named 'program' will be created at ./bin/
+  
+### !Important!
+  - .c files must be on ./src/
+  - .h files must be on ./inc/                 
+  - libs must be on ./lib/
+  
--- makefile_generator.py
+++ makefile_generator.py
@@ -1,22 +1,38 @@
 #!/usr/bin/env python
 
+#################################################################
+#                                                               #
+#                      Makefile Generator                       #
+#          Simple and easy makefile generator made in Python!   #
+#                                                               #
+#    Usage:                                                     #
+#       - set execution permission                              #
+#       - run makefile_generator.py                             #
+#       - make                                                  #
+#       - binary file named 'program' will be created at ./bin/ #
+#                                                               #
+#   !Important!                                                 #
+#       - .c files must be on ./src/                            #
+#       - .h files must be on ./inc/                            #
+#       - libs must be on ./lib/                                #
+#                                                               #
+################################################################
+
 import os
 from fnmatch import fnmatch
 
 pattern = "*.c"
 directory = 'src/'
 
-# Create directories
-
-## bin
+## bin dir
 if not os.path.exists('bin/'):
     os.makedirs('bin/')
 
-## obj
+## obj dir
 if not os.path.exists('obj/'):
     os.makedirs('obj/')
 
-
+# write to makefile
 with open('makefile', 'w') as makefile:
 
     # Compiler
@@ -45,17 +61,23 @@ with open('makefile', 'w') as makefile:
             if fnmatch(name, pattern):
                 objdir = path.replace("src/", "obj/")
                 makefile.write((os.path.join(path, name).replace(".c", ".o").replace("src/", "")) + ' ')
-
+    
+    # Set OBJ dest dir
     makefile.write('\nOBJ = $(patsubst %,$(ODIR)/%,$(_OBJ))\n\n')
+
     # Project compilation
     makefile.write('$(ODIR)/%.o: src/%.c $(DEPS)\n')
     makefile.write('\t\t\t $(CC) -c -o $@ $< $(CFLAGS)\n\n')
+
     # Make algorithm
     makefile.write('default: $(BDIR)/program\n\n')
     makefile.write('$(BDIR)/program: $(OBJ)\n')
     makefile.write('\t\t\t $(CC) -o $@ $^ $(CFLAGS) $(LIBS)\n\n')
-    # PONY
+
+    # PHONY
     makefile.write('.PHONY: clean\n\n')
+
     # Clean
     makefile.write('clean:\n')
     makefile.write('\t\t\t rm -rf $(BDIR)/program\n')
+
